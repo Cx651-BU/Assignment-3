02fd5ebc7f889ddbf394a0dd1cc1f790b079b2b7
Anna Arpaci-Dusseau <anna.arpacidusseau@gmail.com>
2026-08-20 13:35:00 -0400

    initial commit

---
+++ image_calc.c
@@ -0,0 +1,24 @@
+#include "barcode.h"
+#include "loader.h"
+#include <stdlib.h>
+#include <string.h>
+
+
+int main(int argc, char** argv){
+    // TODO: parse the arguments in argv. 
+    // You can expect argv[1] to be the digits to encode
+    // You can expect argv[2] to be the integer width
+    // You can expect argv[3] to be the integer height
+    // You can expect argv[4] to be the output filepath.
+
+    if(argc != 6) {
+        printf("Incorrect number of arguments. Expected: ./build/image_calc <barcode_value> <width> <height> <output_image_path>\n");
+        return -1;
+    }
+
+    // TODO: allocate the space needed for one image.
+
+    // TODO: call barcode
+    
+    // TODO: save result to file
+}
